2ec8783dee2337fd57ecb98583ec728b04b6c1c9
imane-bouabidi <imaneyoucode@gmail.com>
2024-12-12 15:11:47 +0100

    project structure

---
+++ src/test/java/com/wora/MajesticCup/MajesticCupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wora.MajesticCup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajesticCupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
