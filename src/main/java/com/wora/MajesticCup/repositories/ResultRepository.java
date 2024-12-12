1fd578abbd75e3903cedb5abb018a9507c9f3e86
imane-bouabidi <imaneyoucode@gmail.com>
2024-12-12 17:23:56 +0100

    result repository

---
+++ src/main/java/com/wora/MajesticCup/repositories/ResultRepository.java
@@ -0,0 +1,11 @@
+package com.wora.MajesticCup.repositories;
+
+import com.wora.MajesticCup.entities.Result;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ResultRepository extends MongoRepository<Result, Long> {
+    List<Result> findByMatchId(Long matchId);
+    List<Result> findByPlayerId(Long playerId);
+}
