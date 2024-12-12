4ebd3aeb87749e50a737f28540220f5f040f04c5
imane-bouabidi <imaneyoucode@gmail.com>
2024-12-12 17:17:08 +0100

    statistics repository

---
+++ src/main/java/com/wora/MajesticCup/repositories/StatisticsRepository.java
@@ -0,0 +1,11 @@
+package com.wora.MajesticCup.repositories;
+
+import com.wora.MajesticCup.entities.Statistic;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface StatisticsRepository extends MongoRepository<Statistic, Long> {
+    List<Statistic> findByPlayerId(Long playerId);
+}
+
