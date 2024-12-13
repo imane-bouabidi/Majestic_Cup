ae20db35f98a50d2611a7999c8120ef6a7cd7082
imane-bouabidi <imaneyoucode@gmail.com>
2024-12-13 12:24:19 +0100

    Team Mapper and dtos

---
+++ src/main/java/com/wora/MajesticCup/dtos/Team/UpdateTeamDTO.java
@@ -0,0 +1,4 @@
+package com.wora.MajesticCup.dtos.Team;
+
+public class UpdateTeamDTO {
+}
---
+++ src/main/java/com/wora/MajesticCup/mappers/TeamMapper.java
@@ -0,0 +1,13 @@
+package com.wora.MajesticCup.mappers;
+
+import com.wora.MajesticCup.dtos.Team.CreateTeamDTO;
+import com.wora.MajesticCup.dtos.Team.TeamDTO;
+import com.wora.MajesticCup.dtos.Team.UpdateTeamDTO;
+import com.wora.MajesticCup.entities.Team;
+
+public interface TeamMapper {
+    TeamDTO toDTO(Team team);
+    Team toEntity(TeamDTO teamDTO);
+    Team toEntity(CreateTeamDTO createTeamDTO);
+    Team toEntity(UpdateTeamDTO updateTeamDTO);
+}
